3768f12c9e2db8663f00781a8c50c043c6161b33
ABCJIIN <97517088+ABCJIIN@users.noreply.github.com>
2023-05-17 17:53:27 +0900

    0517/17:53/jinny

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
     $("#header").load("../../../html/include/header.html");
 });
+
+
+$(function(){
+    $('#hero').show();
+    $('#banner').hide();
+    $('#heroBtn').click(function(){
+        $('#hero').show();
+        $('#banner').hide();
+        $('#heroBtn').addClass('select').removeClass('non-select');
+        $('#bannerBtn').addClass('non-select').removeClass('select');
+    });
+});
+
+$(function(){
+    $('#bannerBtn').click(function(){
+        $('#banner').show();
+        $('#heroBtn').addClass('non-select').removeClass('select');
+        $('#bannerBtn').addClass('select').removeClass('non-select');
+        $('#hero').hide();
+    });
+});
